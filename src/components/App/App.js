bd0d8a52c72e4f0fa271ba1c11c3fec1a7eceadc
Christina Poole <codenamedchris@gmail.com>
2020-11-07 19:10:23 -0500

    Added props to List

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,23 @@
 import React from "react";
-// import STORE from "../../store";
+import STORE from "../../store";
 import List from "../List/List";
+// import Card from "../Card/Card";
 
 function App() {
+  const lists = STORE.lists;
+  const listTitleArray = lists.map((item) => 
+  <List listTitle={item.header}/> ) //First List ..
+  console.log(lists)
   return (
     <main className="App">
       <header class="App-header">
         <h1>Trelloyes!</h1>
       </header>
       <div class="App-list">
-        <List />
-        <List />
-        <List />
-        <List />
+        {listTitleArray}
       </div>
     </main>
-  );
+  )
 }
 
 export default App;
